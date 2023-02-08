aede4f75474fd7c8bd2b39546acf7370825ffa79
shj <josh2kv@gmail.com>
2023-02-09 01:00:29 +0900

    [mycv] feature signin

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -28,6 +32,20 @@ export class AuthService {
     const user = await this.usersService.create(email, result);
 
     // return the user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) throw new NotFoundException('user not found');
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex'))
+      throw new BadRequestException('bad password');
+
     return user;
   }
 }
--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -29,6 +29,10 @@ export class UsersController {
     return this.authService.signup(body.email, body.password);
   }
 
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
   // @UseInterceptors(ClassSerializerInterceptor)
   // @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
