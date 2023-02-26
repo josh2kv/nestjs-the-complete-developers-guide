abeab3b75e7d634aff96c0add0303400a4b812c9
shj <josh2kv@gmail.com>
2023-02-26 12:11:50 +0900

    [mycv] Add test for signing up with email that is in use

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -5,11 +5,12 @@ import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
 
   // 본격적으로 테스트하기 전 항상 fake users service를 만들도록 함
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeService: Partial<UsersService> = {
+    fakeUserService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -20,7 +21,7 @@ describe('AuthService', () => {
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeService,
+          useValue: fakeUserService,
         },
       ],
     }).compile();
@@ -39,5 +40,16 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    // method를 덮어씌워 다른 response를 받을 수 있음
+    fakeUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+    // for the last version from jest, you can't use `async/await , promise and done together.
+    try {
+      await service.signup('asdf@asdf@.com', 'asdf');
+    } catch (error) {}
   });
 });
